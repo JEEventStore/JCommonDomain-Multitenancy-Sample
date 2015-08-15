59978afe27e5bc0aac9236313b811f8b1292b01a
Alexander Langer <alexander.langer@redrainbow.de>
2015-08-15 20:29:48 +0200

    Simplify AbstractMultiTenancyDomainEvent by extending the one in JCommonDomain

--- ejb/src/main/java/org/jeecqrs/example/multitenancy/domain/common/AbstractMultiTenancyDomainEvent.java
+++ ejb/src/main/java/org/jeecqrs/example/multitenancy/domain/common/AbstractMultiTenancyDomainEvent.java
@@ -1,23 +1,13 @@
 package org.jeecqrs.example.multitenancy.domain.common;
 
-import org.jeecqrs.common.domain.model.AbstractDomainEvent;
 import org.jeecqrs.common.domain.model.multitenancy.MultiTenancyDomainEvent;
 
 public abstract class AbstractMultiTenancyDomainEvent<T> 
-	extends AbstractDomainEvent<T>
+	extends org.jeecqrs.common.domain.model.multitenancy.AbstractMultiTenancyDomainEvent<T, TenantId>
 	implements MultiTenancyDomainEvent<T, TenantId> {
 
-    private final TenantId tenantId;
-
     public AbstractMultiTenancyDomainEvent(TenantId tenantId) {
-        if (tenantId == null)
-            throw new NullPointerException("tenantId must not be null");
-	this.tenantId = tenantId;
-    }
-
-    @Override
-    public final TenantId tenantId() {
-	return tenantId;
+        super(tenantId);
     }
 
 }
